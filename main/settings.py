92ef2f6c4e4d243ed7e221b0c489beb83755f286
Berzezek <wknduz@gmail.com>
2021-07-30 13:09:45 +0500

    FRs

--- main/settings.py
+++ main/settings.py
@@ -154,8 +154,8 @@ REST_FRAMEWORK = {
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 django_heroku.settings(locals())
