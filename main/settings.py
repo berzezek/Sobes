7c572f3d4601b54ebf1695471f23d7ca47389142
Berzezek <wknduz@gmail.com>
2021-07-30 13:08:20 +0500

    FRs

--- main/settings.py
+++ main/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
 #     BASE_DIR, 'static'
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
